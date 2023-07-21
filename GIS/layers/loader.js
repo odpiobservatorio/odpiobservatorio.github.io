76a7a31b512d9a079c7d86a310aef6e3d242aaca
imlargo <jlargob@unal.edu.co>
2023-07-21 11:17:02 -0500

    Cambio en el modo de carga

--- GIS/layers/loader.js
+++ GIS/layers/loader.js
@@ -2,9 +2,5 @@
 
 console.log("........Loading.........")
 
-// Utilizamos fetch para cargar el archivo JSON
-fetch("test.json").then(response => response.json()).then(data => {
-    let tex = (data, null, 2); // Puedes personalizar cómo se muestra la información
-    console.log(tex)
-}).catch(error => console.error("Error al cargar el archivo JSON:", error));
+let text = require("./test.json")
 
