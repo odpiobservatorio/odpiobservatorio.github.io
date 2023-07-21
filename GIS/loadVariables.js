f1b6599a1036adbaac7a2a43d83b56fc4c68c817
imlargo <jlargob@unal.edu.co>
2023-07-21 15:07:41 -0500

    ECOileg converted Js to Json

--- GIS/loadVariables.js
+++ GIS/loadVariables.js
@@ -9,8 +9,9 @@ async function load(path) {
     }
 }
 
-//Cargar las variables de cada base de datos
-(async () => {
+//Cargar las variables de cada base de datos (Luego se puede cambiar a algo mas eficiente)
+(async () => { 
+    //Layers
     let CapaBloquePetrolero = await load('./layers/BloquePetrolero.json');
     let LayerPlano = await load('./layers/Croquis.json');
     let capaDepartamentos = await load('./layers/Departamentos.json');
@@ -19,8 +20,25 @@ async function load(path) {
     let PozosPretoleros = await load('./layers/PozosPet.json');
     let reservasCap = await load('./layers/ReservasCap.json');
     let resguardos = await load('./layers/Resguardos.json');
-    let TitulosMineros = await load('./layers/TitulosMin.json');    
+    let TitulosMineros = await load('./layers/TitulosMin.json');   
+    
+    //ECOIlegal
+    let densidadCoca = await load("./layers/ECOIlegal/desidadCoca.json");
+    let capaFluvialIlegal= await load("./layers/ECOIlegal/FluvilesIlegal.json");
+    let capaRutaArmas = await load("./layers/ECOIlegal/IngArmas.json");
+    let capaContrabando = await load("./layers/ECOIlegal/PuntosContrabando.json");
+    let capaPuntosNarcotrafico = await load("./layers/ECOIlegal/PuntosNarcotrafico.json");
+    let capaRutaMigrantes = await load("./layers/ECOIlegal/RutaMigrantes.json");
 })();
+
+//Los archivos en los que se dividio la informacion ambiental inicializan 3 variables q tienen informacion en formato de string
+//luego se concadenan en la variable ambiental y se convierten en el objeto con toda la informacion
+//Hace lo mismo que tener el archivo gigante, pero se dividio la informacion por el uso del servidor
+let ambiental = JSON.parse(("{" + data1 + data2 +  data3 + "}"));
+
+
+
+
 
 
 
