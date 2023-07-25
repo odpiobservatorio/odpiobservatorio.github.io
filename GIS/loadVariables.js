d75126ad4f0643199a4996d67546a783dcf536fa
imlargo <jlargob@unal.edu.co>
2023-07-24 20:38:28 -0500

    Comments Update

--- GIS/loadVariables.js
+++ GIS/loadVariables.js
@@ -50,7 +50,7 @@ load("./layers/ECOIlegal/PuntosContrabando.json").then(data => { capaContrabando
 load("./layers/ECOIlegal/PuntosNarcotrafico.json").then(data => { capaPuntosNarcotrafico = data });
 load("./layers/ECOIlegal/RutaMigrantes.json").then(data => { capaRutaMigrantes = data });
 
-/*Data principal
+/*
 let DataPrincipal;
 load("../data/DataPrincipal.json").then(data => { DataPrincipal = data });
 */
@@ -59,3 +59,47 @@ let DataPrincipal;
 loadDataBase("1zqI2xhjm3ZxEIr31zkDaDg8_wfCVSBlKgHE2Em6zd0Y", "EVENTOS").then(objeto => {
     DataPrincipal = objeto;
 })
+
+
+/*
+//Futuras Bases de datos online
+
+let DBs = {
+    "BloquePetrolero": "",
+    "Croquis": "",
+    "Departamentos": "",
+    "Fondo": "",
+    "MunicipiosPDET": "",
+    "PozosPet": "",
+    "ReservasCap": "",
+    "Resguardos": "",
+    "TitulosMin": "",
+
+    "desidadCoca": "",
+    "FluvilesIlegal": "",
+    "IngArmas": "",
+    "PuntosContrabando": "",
+    "PuntosNarcotrafico": "",
+    "RutaMigrantes": "",
+}
+
+let id = "1zqI2xhjm3ZxEIr31zkDaDg8_wfCVSBlKgHE2Em6zd0Y";
+
+loadDataBase(id, DBs['BloquePetrolero']).then( Objeto => { CapaBloquePetrolero =  Objeto });
+loadDataBase(id, DBs['Croquis']).then( Objeto => { LayerPlano =  Objeto });
+loadDataBase(id, DBs['Departamentos']).then( Objeto => { capaDepartamentos =  Objeto });
+loadDataBase(id, DBs['Fondo']).then( Objeto => { FondoLayer =  Objeto });
+loadDataBase(id, DBs['MunicipiosPDET']).then( Objeto => { cpaPdet =  Objeto });
+loadDataBase(id, DBs['PozosPet']).then( Objeto => { PozosPretoleros =  Objeto });
+loadDataBase(id, DBs['ReservasCap']).then( Objeto => { reservasCap =  Objeto });
+loadDataBase(id, DBs['Resguardos']).then( Objeto => { resguardos =  Objeto });
+loadDataBase(id, DBs['TitulosMin']).then( Objeto => { TitulosMineros =  Objeto });
+
+//ECOIlegal
+loadDataBase(id, DBs["desidadCoca"]).then( Objeto => { densidadCoca =  Objeto });
+loadDataBase(id, DBs["FluvilesIlegal"]).then( Objeto => { capaFluvialIlegal =  Objeto });
+loadDataBase(id, DBs["IngArmas"]).then( Objeto => { capaRutaArmas =  Objeto });
+loadDataBase(id, DBs["PuntosContrabando"]).then( Objeto => { capaContrabando =  Objeto });
+loadDataBase(id, DBs["PuntosNarcotrafico"]).then( Objeto => { capaPuntosNarcotrafico =  Objeto });
+loadDataBase(id, DBs["RutaMigrantes"]).then( Objeto => { capaRutaMigrantes =  Objeto });
+*/
