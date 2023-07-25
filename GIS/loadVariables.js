bc3cea242bd445668ae1eef56b4687f18bc87d0a
Juan Carlos Largo <134282467+imlargo@users.noreply.github.com>
2023-07-25 10:48:49 -0500

    Update loadVariables.js

--- GIS/loadVariables.js
+++ GIS/loadVariables.js
@@ -55,10 +55,7 @@ let DataPrincipal;
 load("../data/DataPrincipal.json").then(data => { DataPrincipal = data });
 */
 
-let DataPrincipal;
-loadDataBase("1zqI2xhjm3ZxEIr31zkDaDg8_wfCVSBlKgHE2Em6zd0Y", "EVENTOS").then(objeto => {
-    DataPrincipal = objeto;
-})
+
 
 
 /*
