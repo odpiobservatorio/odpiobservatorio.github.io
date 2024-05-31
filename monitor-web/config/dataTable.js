0650dcbc3396b856557a80ab8fe684e10ccb7b36
wilsonlargo <136187461+wilsonlargo@users.noreply.github.com>
2024-05-31 15:43:10 -0500

    Update talba cols

--- monitor-web/config/dataTable.js
+++ monitor-web/config/dataTable.js
@@ -1,4 +1,3 @@
-let data;
 function iniTables() {
     document.getElementById('panel-escritorio').hidden = true;
     document.getElementById('panel-escritorio').hidden = true;
@@ -10,9 +9,9 @@ function iniTables() {
     ActiveDB = clsObservatorio.loadAsInstance(proyectos[0]);
     data = ActiveDB
 
-    makerTable()
+    makerTable(ActiveDB)
 }
-function makerTable() {
+function makerTable(data) {
     const contenedor = document.getElementById('panel-Tablas')
     contenedor.innerHTML = ""
     //Creamos uan tabla general
@@ -21,17 +20,25 @@ function makerTable() {
 
     //========================================================
     const thead = document.createElement("thead")
+    
     thead.innerHTML =
         `
-    <tr>
-        <th>MACROREGIÓN</th>
-        <th>DEPARTAMENTO</th>
-        <th>LUGAR</th>
-        <th>PUEBLO / ÉTNIA</th>
-        <th>MACROTIPO</th>
-        <th>SUB TIPO</th>
-        <th>FECHA</th>
-        <th># AFECTADOS</th>
+    <tr class="sticky-top">
+        <th class="bg-secondary text-white">MACROREGIÓN</th>
+        <th class="bg-secondary text-white">DEPARTAMENTO</th>
+        <th class="bg-secondary text-white">LUGAR</th>
+        <th class="bg-secondary text-white">PUEBLO / ÉTNIA</th>
+        <th class="bg-secondary text-white">MACROTIPO</th>
+        <th class="bg-secondary text-white">SUB TIPO</th>
+        <th class="bg-secondary text-white">FECHA</th>
+        <th class="bg-secondary text-white">MACRO ACTOR</th>
+        <th class="bg-secondary text-white">OTROS ACTORES</th>
+        <th class="bg-secondary text-white">DESPLAZAMIENTO</th>
+        <th class="bg-secondary text-white">AFECTADOS</th>
+        <th class="bg-secondary text-white">MUJERES</th>
+        <th class="bg-secondary text-white">HOMBRES</th>
+        <th class="bg-secondary text-white">MENORES</th>
+        <th class="bg-secondary text-white">ACCIONES</th>
     </tr>
     `
     tableParent.appendChild(thead)
@@ -43,7 +50,6 @@ function makerTable() {
 
     //========================================================
     //POR CADA REGISTRO SE CREA UNA FILA EN LA TABLA TR
-    console.log(data)
     data.clsCasos.forEach(caso => {
         const tr = document.createElement("tr")
 
@@ -78,12 +84,40 @@ function makerTable() {
         td_fecha.textContent = caso.fecha
         tr.appendChild(td_fecha)
 
+        const td_macroactor= document.createElement("td")
+        td_macroactor.style.verticalAlign="middle"
+        td_macroactor.textContent = caso.macroactor
+        tr.appendChild(td_macroactor)
+
+        const td_actores = _crearCelda.data_actores(caso)
+        tr.appendChild(td_actores)
+
+        const td_desplazamiento = _crearCelda.data_desplazamiento(caso)
+        tr.appendChild(td_desplazamiento)
+
         const td_tpersonas= document.createElement("td")
         td_tpersonas.style.verticalAlign="middle"
         td_tpersonas.textContent = caso.npersonas
         tr.appendChild(td_tpersonas)
 
+        const td_tmujeres= document.createElement("td")
+        td_tmujeres.style.verticalAlign="middle"
+        td_tmujeres.style.backgroundColor="pink"
+        td_tmujeres.textContent = caso.nmujeres
+        tr.appendChild(td_tmujeres)
 
+        const td_thombres= document.createElement("td")
+        td_thombres.style.verticalAlign="middle"
+        td_thombres.textContent = caso.nhombres
+        tr.appendChild(td_thombres)
+
+        const td_tmenores= document.createElement("td")
+        td_tmenores.style.verticalAlign="middle"
+        td_tmenores.textContent = caso.nmenores
+        tr.appendChild(td_tmenores)
+
+        const td_acciones = _crearCelda.data_acciones(caso)
+        tr.appendChild(td_acciones)
 
 
 
@@ -101,8 +135,7 @@ const _crearCelda = {
     data_lugar(parent) {
 
         const acordeon= document.createElement("div")
-        acordeon.className="accordion"
-        acordeon.id=`accordion${parent.id}`
+        acordeon.className=""
 
         const acordeonitem= document.createElement("div")
         acordeonitem.className="accordion-item"
@@ -115,7 +148,7 @@ const _crearCelda = {
         const accordionbutton= document.createElement("div")
         accordionbutton.innerHTML=
         `
-                    <button class="accordion-button border-0 border" type="button" style="background-color: white;" 
+                    <button class="accordion-button ps-2 border-1 rounded bg-warning-subtle" type="button"
                                     data-bs-toggle="collapse" 
                                     data-bs-target="#collapse${parent.id}" 
                                     aria-expanded="true" 
@@ -133,19 +166,20 @@ const _crearCelda = {
         parent.clsLugares.forEach(lugar=>{
             const itemlugar = document.createElement("div")
             itemlugar.textContent=lugar.municipio
-            itemlugar.className="label-org-gray-light"
+            itemlugar.className="m-2"
             collapse.appendChild(itemlugar)
 
         })
         const td= document.createElement("td")
-        td.appendChild(acordeon)
+        if (parent.clsLugares.length!=0){
+            td.appendChild(acordeon)
+        }
         return td
     },
     data_pueblo(parent){
 
         const acordeon= document.createElement("div")
-        acordeon.className="accordion"
-        acordeon.id=`accordionpueblo${parent.id}`
+        acordeon.className=""
 
         const acordeonitem= document.createElement("div")
         acordeonitem.className="accordion-item"
@@ -158,12 +192,12 @@ const _crearCelda = {
         const accordionbutton= document.createElement("div")
         accordionbutton.innerHTML=
         `
-                    <button class="accordion-button border-0 border" type="button" style="background-color: white;" 
+                    <button class="accordion-button ps-2 border-1 rounded bg-warning-subtle" type="button" 
                                     data-bs-toggle="collapse" 
                                     data-bs-target="#collapsepueblo${parent.id}" 
                                     aria-expanded="true" 
                                     >
-                        Pueblos ${parent.clsPueblos.length}
+                        Pueblos (${parent.clsPueblos.length})
                     </button>
         `
         acordeon.appendChild(accordionbutton)
@@ -176,19 +210,21 @@ const _crearCelda = {
         parent.clsPueblos.forEach(pueblo=>{
             const itempueblo = document.createElement("div")
             itempueblo.textContent=pueblo.nombre
-            itempueblo.className="label-org-gray-light"
+            itempueblo.className="m-2"
             collapse.appendChild(itempueblo)
 
         })
         const td= document.createElement("td")
-        td.appendChild(acordeon)
+        if (parent.clsPueblos.length!=0){
+            td.appendChild(acordeon)
+        }
         return td
 
     },
     data_tipos(parent){
 
         const acordeon= document.createElement("div")
-        acordeon.className="accordion"
+        acordeon.className=""
 
         const acordeonitem= document.createElement("div")
         acordeonitem.className="accordion-item"
@@ -201,7 +237,7 @@ const _crearCelda = {
         const accordionbutton= document.createElement("div")
         accordionbutton.innerHTML=
         `
-                    <button class="accordion-button border-0 border" type="button" style="background-color: white;" 
+                    <button class="accordion-button ps-2 border-1 rounded bg-warning-subtle" type="button"
                                     data-bs-toggle="collapse" 
                                     data-bs-target="#collapsetipo${parent.id}" 
                                     aria-expanded="true" 
@@ -219,13 +255,151 @@ const _crearCelda = {
         parent.clsTipos.forEach(tipo=>{
             const item = document.createElement("div")
             item.textContent=tipo.nombre
-            item.className="label-org-gray-light"
+            item.className="m-2"
             collapse.appendChild(item)
 
         })
         const td= document.createElement("td")
-        td.appendChild(acordeon)
+        if (parent.clsTipos.length!=0){
+            td.appendChild(acordeon)
+        }
+        return td
+
+    },
+    data_actores(parent){
+
+        const acordeon= document.createElement("div")
+        acordeon.className=""
+
+        const acordeonitem= document.createElement("div")
+        acordeonitem.className="accordion-item"
+        acordeon.appendChild(acordeonitem)
+
+        const accordionheader= document.createElement("h3")
+        accordionheader.className="accordion-header"
+        acordeon.appendChild(accordionheader)
+
+        const accordionbutton= document.createElement("div")
+        accordionbutton.innerHTML=
+        `
+                    <button class="accordion-button ps-2 border-1 rounded bg-warning-subtle" type="button"
+                                    data-bs-toggle="collapse" 
+                                    data-bs-target="#collapseactor${parent.id}" 
+                                    aria-expanded="true" 
+                                    >
+                        Actores (${parent.clsActores.length})
+                    </button>
+        `
+        acordeon.appendChild(accordionbutton)
+
+        const collapse= document.createElement("div")
+        collapse.className="accordion-collapse collapse"
+        collapse.id="collapseactor" + parent.id
+        acordeon.appendChild(collapse)
+
+        parent.clsActores.forEach(actor=>{
+            const item = document.createElement("div")
+            item.textContent=actor.nombre
+            item.className="m-2"
+            collapse.appendChild(item)
+
+        })
+        const td= document.createElement("td")
+        if (parent.clsActores.length!=0){
+            td.appendChild(acordeon)
+        }
+        return td
+
+    },
+    data_desplazamiento(parent){
+
+        const acordeon= document.createElement("div")
+        acordeon.className=""
+
+        const acordeonitem= document.createElement("div")
+        acordeonitem.className="accordion-item"
+        acordeon.appendChild(acordeonitem)
+
+        const accordionheader= document.createElement("h3")
+        accordionheader.className="accordion-header"
+        acordeon.appendChild(accordionheader)
+
+        const accordionbutton= document.createElement("div")
+        accordionbutton.innerHTML=
+        `
+                    <button class="accordion-button ps-2 border-1 rounded bg-warning-subtle" type="button"
+                                    data-bs-toggle="collapse" 
+                                    data-bs-target="#collapseactor${parent.id}" 
+                                    aria-expanded="true" 
+                                    >
+                        Hechos (${parent.clsDesplazamiento.length})
+                    </button>
+        `
+        acordeon.appendChild(accordionbutton)
+
+        const collapse= document.createElement("div")
+        collapse.className="accordion-collapse collapse"
+        collapse.id="collapseactor" + parent.id
+        acordeon.appendChild(collapse)
+
+        parent.clsDesplazamiento.forEach(hecho=>{
+            const item = document.createElement("div")
+            item.textContent=hecho.tipo
+            item.className="m-2"
+            collapse.appendChild(item)
+
+        })
+        const td= document.createElement("td")
+        if (parent.clsDesplazamiento.length!=0){
+            td.appendChild(acordeon)
+        }
+        return td
+
+    },
+    data_acciones(parent){
+
+        const acordeon= document.createElement("div")
+        acordeon.className=""
+
+        const acordeonitem= document.createElement("div")
+        acordeonitem.className="accordion-item"
+        acordeon.appendChild(acordeonitem)
+
+        const accordionheader= document.createElement("h3")
+        accordionheader.className="accordion-header"
+        acordeon.appendChild(accordionheader)
+
+        const accordionbutton= document.createElement("div")
+        accordionbutton.innerHTML=
+        `
+                    <button class="accordion-button ps-2 border-1 rounded bg-warning-subtle" type="button"
+                                    data-bs-toggle="collapse" 
+                                    data-bs-target="#collapseaccion${parent.id}" 
+                                    aria-expanded="true" 
+                                    >
+                        Acciones (${parent.clsAccJuridica.length})
+                    </button>
+        `
+        acordeon.appendChild(accordionbutton)
+
+        const collapse= document.createElement("div")
+        collapse.className="accordion-collapse collapse"
+        collapse.id="collapseaccion" + parent.id
+        acordeon.appendChild(collapse)
+
+        parent.clsAccJuridica.forEach(accion=>{
+            const item = document.createElement("div")
+            item.textContent=accion.accion
+            item.className="m-2"
+            collapse.appendChild(item)
+
+        })
+        const td= document.createElement("td")
+        if (parent.clsAccJuridica.length!=0){
+            td.appendChild(acordeon)
+        }
         return td
 
     }
+
 }
